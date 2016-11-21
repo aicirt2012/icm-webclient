63dea536c4320a28ea8375c05d30e190d8929857
Peter <peter@niedermeier-ed.de>
2016-11-21 17:21:38 +0100

    Implemented Trello profile component

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'profile',
-  template: `
-    <div>
-      <h1>404: page missing</h1>
-    </div>
-  `
+  templateUrl: 'profile.component.html',
+  styleUrls: ['profile.component.css'],
 })
 export class ProfileComponent {
+  model: any = {};
+  loading = false;
+  error = '';
 
+  constructor(
+    private router: Router,
+    private _auth: AuthService) { }
+
+  ngOnInit() {
+    // reset login status
+    this._auth.logout();
+  }
+
+  login() {
+    this.loading = true;
+    this._auth.login(this.model.username, this.model.password)
+      .subscribe(result => {
+        if (result === true) {
+          // login successful
+          this.router.navigate(['/']);
+        } else {
+          // login failed
+          this.error = 'Username or password is incorrect';
+          this.loading = false;
+        }
+      });
+  }
 }
