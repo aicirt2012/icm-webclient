35946a60a4e0902541b45402b651a54cc1652793
Daniel Elsner <daniel.elsner@tum.de>
2016-12-10 13:13:49 +0100

    update shared module deps

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+import { Angular2FlexModule } from 'angular2-flex';
 import { SpinnerComponent, TopNavbarComponent } from './components'; // all shared components
 import { AuthService, HttpService } from './services';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
-  imports: [RouterModule, BrowserModule, FormsModule],
+  imports: [RouterModule, BrowserModule, FormsModule, Ng2BootstrapModule, Angular2FlexModule],
   declarations: [
     SpinnerComponent,
     TopNavbarComponent],
