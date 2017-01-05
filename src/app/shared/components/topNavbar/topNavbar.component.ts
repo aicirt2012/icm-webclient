5a64067928eddbe6be2f6be2e17b3bf9f607d593
Daniel Elsner <daniel.elsner@tum.de>
2017-01-05 17:37:28 +0100

    fix navbar

--- src/app/shared/components/topNavbar/topNavbar.component.ts
+++ src/app/shared/components/topNavbar/topNavbar.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['topNavbar.component.css'],
 })
 export class TopNavbarComponent {
-  public items: any[] = [{ name: 'Settings', route: '/settings/email' }, { name: 'Logout', route: '/login' }];
+  public items: any[] = [{ name: 'Settings', route: '/settings/email', icon: 'account_circle' }, { name: 'Logout', route: '/login', icon: 'exit_to_app' }];
 
   constructor() { }
 
