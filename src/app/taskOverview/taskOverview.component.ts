c3d2203855074e5d2b207e261da517d46fc8a5be
cmense <constantin.mense@t-online.de>
2017-02-08 12:49:12 +0100

    Fix error with draft mails Fix error with endless loop empty box Fix catch error task overview when no task provider is connected

--- src/app/taskOverview/taskOverview.component.ts
+++ src/app/taskOverview/taskOverview.component.ts
@@ -1,3 +1,6 @@
+import { SettingsService } from './../settings/shared/settings.service';
+import { User } from './../shared/models/user.model';
+import { AppState } from './../app.service';
 import { TaskService } from './../client/shared/task.service';
 import { Component } from '@angular/core';
 import { AuthService } from '../shared';
@@ -11,19 +14,24 @@ import C from '../shared/constants';
   styleUrls: ['taskOverview.component.css'],
 })
 export class TaskOverviewComponent {
-  boards: any[] =[];
+  boards: any[] = [];
   fetching: boolean = false;
+  user: any = {};
 
-  constructor(private _taskService: TaskService) { }
+  constructor(private _taskService: TaskService, private _settingsService: SettingsService) { }
 
   ngOnInit() {
-    this.fetching = true;
-    this._taskService.getAllBoards({linkedTasks: true}).subscribe((boards) => {
-      this.boards = boards;
-      this.fetching = false;
-      console.log(this.boards);
-    });
-  
-}
+    this._settingsService.getUserInfo().subscribe((user) => {
+      this.user = user;
+      console.log(user);
+      if (this.user.trello) {
+      this.fetching = true;
+      this._taskService.getAllBoards({ linkedTasks: true }).subscribe((boards) => {
+        this.boards = boards;
+        this.fetching = false;
+      });
+    }
+    })
+  }
 
 }
