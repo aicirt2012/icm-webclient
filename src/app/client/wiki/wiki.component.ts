3205c7f9295f8d727faea1aea1bcefc5dae7ab18
Felix Michel <felix.michel@tum.de>
2017-02-24 09:12:01 +0100

    wiki content links style

--- src/app/client/wiki/wiki.component.ts
+++ src/app/client/wiki/wiki.component.ts
@@ -9,7 +9,7 @@ import { WikiService } from '../shared/wiki.service'
 
 export class WikiComponent{
  
-  private text: any;
+  private content: any;
   private query: string = 'Munich';
 
   constructor(private ws: WikiService, private element: ElementRef) {
@@ -20,7 +20,7 @@ export class WikiComponent{
 
   search(){
     this.ws.search(this.query).subscribe(data=>{
-      this.text = data.teaser;       
+      this.content = data.teaser;       
       window.setTimeout(()=>{
         this.addEvents();       
       },10);
@@ -30,12 +30,20 @@ export class WikiComponent{
   addEvents() {
     const me = this;
     me.element.nativeElement.querySelectorAll('.wiki-link').forEach((e)=> {      
-      e.addEventListener('click', function(){              
+      e.addEventListener('click', ()=>{              
         me.query = e.getAttribute('title');
         me.search();
       });
       e.removeAttribute('href','#');
-      e.setAttribute('style', 'color:#666;');
+      const baseStyle = 'color:#666;';
+      const hoverStyle = 'color:red; cursor:pointer;';
+      e.setAttribute('style', baseStyle);
+      e.addEventListener('mouseover', ()=>{
+        e.setAttribute('style', hoverStyle);
+      });
+      e.addEventListener('mouseout', ()=>{
+        e.setAttribute('style', baseStyle);
+      });            
     });
   }
 
