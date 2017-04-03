58a493503d536db72c88f96dfa6b1d5549048cbc
Felix Michel <felix.michel@tum.de>
2017-04-03 22:22:25 +0200

    client refactoring

--- src/app/client/components/index.ts
+++ src/app/client/components/index.ts
@@ -2,5 +2,4 @@ export * from './emailForm';
 export * from './taskListItem';
 export * from './sentenceDialog';
 export * from './taskItemEmailDialog';
-export * from './emailResponse';
 export * from './taskActionBar';
--- src/app/client/emailDetailedView/index.ts
+++ src/app/client/emailDetailedView/index.ts
@@ -1,3 +1,4 @@
 export * from './emailDetailedView.component';
 export * from './emailView';
 export * from './emailActionBar';
+export * from './emailResponse';
