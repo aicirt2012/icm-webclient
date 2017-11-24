91925f4811ba4951fb49f2f0dd31fd6a790edc06
sunborn23 <github@sunborn.me>
2017-11-24 03:31:39 +0100

    implemented proof of concept for custom annotator storage module

--- annotator-test/annotator.js
+++ annotator-test/annotator.js
@@ -59,13 +59,13 @@ module.exports = ".annotator-filter *,.annotator-notice,.annotator-widget *{font
   }
 })(function () {
   "use strict";
-  
+
   // mini-underscore
   var _ = {
     has: function (obj, key) {
       return Object.prototype.hasOwnProperty.call(obj, key);
     },
-  
+
     extend: function(obj) {
       for (var i=1; i<arguments.length; ++i) {
         var source = arguments[i];
@@ -447,7 +447,7 @@ function polyfill() {
     local = self;
   }
 
-  var es6PromiseSupport = 
+  var es6PromiseSupport =
     "Promise" in local &&
     // Some of these methods are missing from
     // Firefox/Chrome experimental implementations
@@ -895,7 +895,7 @@ var inserted = {};
 module.exports = function (css, options) {
     if (inserted[css]) return;
     inserted[css] = true;
-    
+
     var elem = document.createElement('style');
     elem.setAttribute('type', 'text/css');
 
@@ -904,7 +904,7 @@ module.exports = function (css, options) {
     } else {
       elem.styleSheet.cssText = css;
     }
-    
+
     var head = document.getElementsByTagName('head')[0];
     if (options && options.prepend) {
         head.insertBefore(elem, head.childNodes[0]);
@@ -12476,6 +12476,99 @@ exports.debug = function () {
         }
     };
 };
+
+
+  /**
+   * function:: parentwindow()
+   * TODO move this custom module to an external js file and make it available to the framework
+   *
+   * A storage component that can be used to communicate details of the annotation
+   * to the parent window when Annotator is rin in e.g. an iFrame.
+   *
+   * Use as an extension module::
+   *
+   *     app.include(annotator.storage.parentwindow);
+   *
+   */
+  exports.parentwindow = function () {
+    function trace(action, annotation) {
+      var copyAnno = JSON.parse(JSON.stringify(annotation));
+      console.debug("annotator.storage.parentwindow: " + action, copyAnno);
+    }
+
+    return {
+      create: function (annotation) {
+        // annotation.id = id();
+        trace('create (not yet implemented)', annotation);
+        return annotation;
+      },
+
+      update: function (annotation) {
+        trace('update (not yet implemented)', annotation);
+        return annotation;
+      },
+
+      'delete': function (annotation) {
+        trace('destroy (not yet implemented)', annotation);
+        return annotation;
+      },
+
+      query: function (queryObj) {
+        trace('query', queryObj);
+        return {
+          meta: {total: 1},
+          results: [
+            {
+              "quote": "Google",
+              "text": "test_injection",
+              "ranges": [
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[2]/td[1]",
+                  "endOffset": 89,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[2]/td[1]",
+                  "startOffset": 83
+                },
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[2]/td[1]",
+                  "endOffset": 499,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[2]/td[1]",
+                  "startOffset": 493
+                },
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[3]/td[1]",
+                  "endOffset": 35,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[3]/td[1]",
+                  "startOffset": 29
+                },
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[5]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/a[1]",
+                  "endOffset": 6,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[4]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[5]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/a[1]",
+                  "startOffset": 0
+                },
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[7]/td[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]",
+                  "endOffset": 13,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[7]/td[1]/table[1]/tbody[1]/tr[3]/td[1]/div[1]",
+                  "startOffset": 7
+                },
+                {
+                  "end": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[7]/td[1]/table[1]/tbody[1]/tr[1]/td[1]",
+                  "endOffset": 125,
+                  "start": "/table[1]/tbody[1]/tr[2]/td[2]/table[1]/tbody[1]/tr[7]/td[1]/table[1]/tbody[1]/tr[1]/td[1]",
+                  "startOffset": 119
+                }
+              ]
+            }
+          ]
+        };
+      },
+
+      configure: function (registry) {
+        registry.registerUtility(this, 'storage');
+      }
+    };
+  };
 
 
 /**
