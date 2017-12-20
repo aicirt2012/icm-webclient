2ead09b84e2fb8cf712e31fdf2e1421b5104ad83
Simon Bönisch <github@sunborn.me>
2017-12-20 21:10:10 +0100

    started restoring initial translate/wikipedia functionality

--- src/assets/js/annotator.custom-extensions.js
+++ src/assets/js/annotator.custom-extensions.js
@@ -104,6 +104,9 @@ annotatorCustomExtensions.injectCustomElements = function () {
   function getButtonTranslate() {
     var customButton = document.createElement("div");
     customButton.classList.add("annotator-custom-action");
+    customButton.setAttribute("onclick", "debugger;");
+    //TODO parse current selection into an annotation using the internal annotator.js method for doing so
+    // customButton.setAttribute("onclick", "document.dispatchEvent(new CustomEvent(\"OnTranslationClick\",{\"detail\":this.annotation.quote}));");
     // var buttonIcon = document.createElement("i");
     // buttonIcon.classList.add("material-icons");
     // buttonIcon.appendChild(document.createTextNode("translate"));
@@ -115,6 +118,7 @@ annotatorCustomExtensions.injectCustomElements = function () {
   function getButtonWikipedia() {
     var customButton = document.createElement("div");
     customButton.classList.add("annotator-custom-action");
+    customButton.setAttribute("onclick", "document.dispatchEvent(new CustomEvent(\"OnSearchClick\",{\"detail\":this.annotation.quote}));");
     // var buttonIcon = document.createElement("i");
     // buttonIcon.classList.add("material-icons");
     // buttonIcon.appendChild(document.createTextNode("wikipedia"));
