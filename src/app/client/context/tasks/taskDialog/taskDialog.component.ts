fbb9a0077da7b09e6c33b7c77854f36ccec555ab
Simon Bönisch <github@sunborn.me>
2018-06-18 12:00:01 +0200

    added skeleton for task dialog

---
+++ src/app/client/context/tasks/taskDialog/taskDialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'task-dialog',
+  styleUrls: ['./taskDialog.component.css'],
+  templateUrl: './taskDialog.component.html'
+})
+
+export class TaskDialogComponent {
+
+}
