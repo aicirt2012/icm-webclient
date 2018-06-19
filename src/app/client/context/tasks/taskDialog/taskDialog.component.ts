285e17a118a84b4bff1936f739fe21a5599a17a0
Simon Bönisch <github@sunborn.me>
2018-06-19 14:25:44 +0200

    started implementing task dialog

--- src/app/client/context/tasks/taskDialog/taskDialog.component.ts
+++ src/app/client/context/tasks/taskDialog/taskDialog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatDialogRef } from "@angular/material";
 
 @Component({
   selector: 'task-dialog',
@@ -11,5 +12,19 @@ export class TaskDialogComponent {
   public task: any;
   public suggestedData: any[] = [];
   public isSuggestion: boolean = false;
+
+  private title: string = "(Untitled Task)";
+  private provider: string = "";
+  private dueDate: string;
+  private members: any[] = [];
+  private state: string;
+
+
+  constructor(public taskDialogRef: MatDialogRef<TaskDialogComponent>) {
+  }
+
+  closeDialog() {
+    this.taskDialogRef.close();
+  }
 
 }
