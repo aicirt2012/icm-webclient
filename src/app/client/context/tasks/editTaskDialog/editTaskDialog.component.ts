7846b6aa010e3a86766c4fddb11a3932a052132b
Simon Bönisch <github@sunborn.me>
2018-06-20 02:02:26 +0200

    started implementing editDialog for trello

--- src/app/client/context/tasks/editTaskDialog/editTaskDialog.component.ts
+++ src/app/client/context/tasks/editTaskDialog/editTaskDialog.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialogRef } from "@angular/material";
+import { TaskService } from "../../../shared";
 
 @Component({
   selector: 'edit-task-dialog',
@@ -11,17 +12,47 @@ export class EditTaskDialogComponent {
 
   public task: any;
 
-  private title: string = "(Untitled Task)";
-  private provider: string = "";
+  private title: string;
+  private provider: string;
+  private dueDateRaw: any;
   private dueDate: string;
-  private members: any[] = [];
-  private state: string;
+
+  private trelloData: any;
+  private sacmData: any;
 
   constructor(public taskDialogRef: MatDialogRef<EditTaskDialogComponent>) {
   }
 
+  ngOnInit() {
+    this.title = TaskService.getParameter(this.task, 'name');
+    this.provider = this.task.provider;
+    this.dueDateRaw = TaskService.getParameter(this.task, 'due');
+
+    if (this.dueDateRaw) {
+      this.dueDateRaw = new Date(this.dueDateRaw);
+      this.dueDate = TaskService.formatDate(this.dueDateRaw);
+    }
+
+    if (this.provider === "trello")
+      this.initTrelloTask();
+    if (this.provider === "sociocortex")
+      this.initSacmTask();
+  }
+
   closeDialog() {
     this.taskDialogRef.close();
+  }
+
+  private initTrelloTask() {
+    this.trelloData = {
+      boardId: TaskService.getParameter(this.task, 'boardId')
+    };
+  }
+
+  private initSacmTask() {
+    this.sacmData = {
+      caseId: TaskService.getParameter(this.task, 'case')
+    };
   }
 
 }
