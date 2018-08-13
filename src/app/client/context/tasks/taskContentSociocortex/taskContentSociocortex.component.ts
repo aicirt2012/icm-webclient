2ea8fd366ef2eb9cfd40ecb0d2801c34495acf26
Simon Bönisch <github@sunborn.me>
2018-08-13 19:23:23 +0200

    unified content components after trello implementation

--- src/app/client/context/tasks/taskContentSociocortex/taskContentSociocortex.component.ts
+++ src/app/client/context/tasks/taskContentSociocortex/taskContentSociocortex.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'task-content-sociocortex',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 
 export class TaskContentSociocortexComponent {
+
+  @Input()
+  private contentForm: FormGroup;
 
 }
 
