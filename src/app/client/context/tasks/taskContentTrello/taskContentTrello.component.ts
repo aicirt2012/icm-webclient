0a3f62908b4136928137e91678101d77f7f0c3de
Simon Bönisch <github@sunborn.me>
2018-08-13 19:20:38 +0200

    implemented trello description input via separate component

--- src/app/client/context/tasks/taskContentTrello/taskContentTrello.component.ts
+++ src/app/client/context/tasks/taskContentTrello/taskContentTrello.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'task-content-trello',
@@ -7,6 +8,9 @@ import { Component } from '@angular/core';
 })
 
 export class TaskContentTrelloComponent {
+
+  @Input()
+  private contentForm: FormGroup;
 
 }
 
