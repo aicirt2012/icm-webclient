6df8be3e1636bf8f0a8b39a6bb294c978ae5a727
Simon Bönisch <github@sunborn.me>
2018-08-17 04:19:51 +0200

    implemented html element prediction

--- src/app/client/context/tasks/taskContentSociocortex/taskContentSociocortex.component.ts
+++ src/app/client/context/tasks/taskContentSociocortex/taskContentSociocortex.component.ts
@@ -11,12 +11,52 @@ export class TaskContentSociocortexComponent {
 
   @Input()
   private contentForm: FormGroup;
-  @Input()
-  private taskParams: any[];
+  private _taskParams: any[];
 
-  ngOnInit() {
-    console.log("init sociocortex content. params:");
-    console.log(this.taskParams);
+  get taskParams(): any[] {
+    return this._taskParams;
+  }
+
+  @Input()
+  set taskParams(taskParams: any[]) {
+    this._taskParams = taskParams;
+    this.preProcessParameters();
+    console.log("Preprocessed parameters.", this._taskParams);
+  }
+
+  private preProcessParameters() {
+    this._taskParams.forEach(taskParam => {
+      switch (taskParam.type) {
+        case "enumeration":
+          if (taskParam.multiplicity === 'exactlyOne')
+            taskParam.htmlElement = "radioboxes";
+          else if (taskParam.multiplicity === 'any')
+            taskParam.htmlElement = "checkboxes";
+          else
+            console.error("Unknown multiplicity for parameter type enumeration", taskParam);
+          break;
+        case "string":
+          if (taskParam.multiplicity === 'exactlyOne')
+            taskParam.htmlElement = "textinput";
+          else
+            console.error("Unknown multiplicity for parameter type string", taskParam);
+          break;
+        case "date":
+          if (taskParam.multiplicity === 'exactlyOne')
+            taskParam.htmlElement = "datepicker";
+          else
+            console.error("Unknown multiplicity for parameter type date", taskParam);
+          break;
+        case "link":
+          if (taskParam.multiplicity === 'exactlyOne')
+            taskParam.htmlElement = "select";
+          else
+            console.error("Unknown multiplicity for parameter type link", taskParam);
+          break;
+        default:
+          console.error("Unknown parameter type", taskParam)
+      }
+    });
   }
 
 }
