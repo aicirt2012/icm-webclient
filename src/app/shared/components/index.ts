20ab55cb8afc1734bd5994da94fb647c1c80b178
Simon Bönisch <github@sunborn.me>
2018-08-30 20:18:24 +0200

    added default exports

--- src/app/shared/components/index.ts
+++ src/app/shared/components/index.ts
@@ -1,2 +1,3 @@
+export * from './datePicker';
 export * from './spinner';
 export * from './topNavbar';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,32 +1,32 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import {
   MatAutocompleteModule,
   MatButtonModule,
   MatButtonToggleModule,
+  MatCardModule,
   MatCheckboxModule,
+  MatChipsModule,
   MatDialogModule,
+  MatFormFieldModule,
   MatIconModule,
   MatInputModule,
   MatListModule,
   MatMenuModule,
+  MatSelectModule,
   MatSliderModule,
   MatSlideToggleModule,
-  MatToolbarModule,
-  MatTooltipModule,
-  MatSelectModule,
-  MatCardModule,
-  MatChipsModule,
+  MatSnackBarModule,
   MatTabsModule,
-  MatFormFieldModule,
-  MatSnackBarModule
+  MatToolbarModule,
+  MatTooltipModule
 } from '@angular/material';
-import {FlexLayoutModule} from "@angular/flex-layout";
-import {SpinnerComponent, TopNavbarComponent} from './components'; // all shared components
-import {DisableControlDirective} from './directives'; // all shared directives
-import {AuthService, HttpService} from './services';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { DatePickerComponent, SpinnerComponent, TopNavbarComponent } from './components'; // all shared components
+import { DisableControlDirective } from './directives'; // all shared directives
+import { AuthService, HttpService } from './services';
 
 @NgModule({
   imports: [
@@ -57,11 +57,13 @@ import {AuthService, HttpService} from './services';
     FlexLayoutModule
   ],
   declarations: [
+    DatePickerComponent,
     SpinnerComponent,
     TopNavbarComponent,
     DisableControlDirective
   ],
   exports: [
+    DatePickerComponent,
     SpinnerComponent,
     TopNavbarComponent,
     DisableControlDirective
