62dda1ba3995ac68b6e5c8df13d33849313cb1b8
Simon Bönisch <github@sunborn.me>
2018-08-30 20:11:52 +0200

    added datepicker component skeleton

---
+++ src/app/shared/components/datePicker/datePicker.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'datepicker',
+  templateUrl: './datePicker.component.html',
+  styleUrls: ['./datePicker.component.css']
+})
+export class DatePickerComponent {
+
+  constructor() {
+  }
+
+}
