f895d979220f09d2a851b4ab08f738edba0836a9
Simon Bönisch <github@sunborn.me>
2018-08-30 20:37:51 +0200

    started implementing

--- src/app/shared/components/datePicker/datePicker.component.ts
+++ src/app/shared/components/datePicker/datePicker.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'datepicker',
@@ -7,7 +7,30 @@ import { Component } from '@angular/core';
 })
 export class DatePickerComponent {
 
+  @Input()
+  private value: Date;
+  @Input()
+  private suggestedValues: Date[];
+  private displayedValue: MyDate;
+  private displayedSuggestions: MyDate[];
+
   constructor() {
   }
 
+}
+
+class MyDate {
+  value: Date;
+
+  toString(): string {
+    if (!this.value)
+      return "";
+    let d = new Date(this.value),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    return [year, month, day].join('-');
+  }
 }
