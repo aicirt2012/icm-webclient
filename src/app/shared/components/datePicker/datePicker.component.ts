e09f96aa0aa7b13c0172017df6b17077134bec01
Simon Bönisch <github@sunborn.me>
2018-09-01 20:03:23 +0200

    refactored: simplified; added user input validation

--- src/app/shared/components/datePicker/datePicker.component.ts
+++ src/app/shared/components/datePicker/datePicker.component.ts
@@ -8,8 +8,8 @@ import { FormControl } from '@angular/forms';
 })
 export class DatePickerComponent {
 
-  @Input()
-  private autocompleteDates: Date[];
+  // noinspection JSMismatchedCollectionQueryUpdate
+  @Input() private autocompleteDates: Date[];
   private stringValue: FormControl = new FormControl('');
   private dateValue: FormControl = new FormControl(null);
   private autocomplete = {
@@ -24,33 +24,30 @@ export class DatePickerComponent {
     // TODO init the views from the values we got
     console.log("init date picker component. stringValue, dateValue, autocomplete", this.stringValue.value, this.dateValue.value, this.autocomplete);
 
-    this.autocomplete.all = this.autocompleteDates.map(date => new MyDate(date).toString());
+    this.autocomplete.all = this.autocompleteDates.map(date => DatePickerComponent.dateToString(date));
     this.autocomplete.filtered = this.autocomplete.all;
     this.stringValue.valueChanges
       .subscribe(updatedValue => {
         this.autocomplete.filtered = this.autocomplete.all.filter(date => date.indexOf(updatedValue) == 0);
       });
     this.dateValue.valueChanges
-      .subscribe(() => this.stringValue.setValue(new MyDate(this.dateValue.value).toString()));
+      .subscribe(() => this.stringValue.setValue(DatePickerComponent.dateToString(this.dateValue.value)));
   }
 
   onStringInputBlur() {
-    this.dateValue.setValue(new Date(this.stringValue.value));
+    const date = new Date(this.stringValue.value);
+    if (date.getTime() > 0)   // check if value is valid date
+      this.dateValue.setValue(date);
+    else {
+      this.stringValue.setValue("");  // TODO improve error handling by keeping the input and showing a validation error
+      this.dateValue.setValue(null);
+    }
   }
 
-}
-
-class MyDate {
-  value: Date;
-
-  constructor(value: Date) {
-    this.value = value;
-  }
-
-  toString(): string {
-    if (!this.value)
+  private static dateToString(date: Date): string {
+    if (!date)
       return "";
-    let d = new Date(this.value),
+    let d = new Date(date),
       month = '' + (d.getMonth() + 1),
       day = '' + d.getDate(),
       year = d.getFullYear();
@@ -58,4 +55,5 @@ class MyDate {
     if (day.length < 2) day = '0' + day;
     return [year, month, day].join('-');
   }
+
 }
